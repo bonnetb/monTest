0a276871d9570dddc486712898022bfdfaa7477e
Benjamin BONNET <benjamin.bonnet@m4x.org>
2018-06-14 10:24:39 +0200

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -2,10 +2,12 @@ package hello;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.beans.factory.annotation.Value;
 @RestController
 public class HelloController {
-
+    @Value("qui")
+    private String qui;
+    
     @RequestMapping("/")
     public String index() {
         return "Bonjour!";
